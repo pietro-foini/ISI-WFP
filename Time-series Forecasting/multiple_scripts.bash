b1b4fe4854fe9403515f8996fe53db57be941c96
Pietro Foini <pietro.foini1@gmail.com>
2022-06-11 13:54:35 +0200

    Add new analysis 7 provinces

---
+++ Time-series Forecasting/multiple_scripts.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Usage: bash multiple_scripts.bash
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/0/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "0"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/1/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "1"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/2/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "2"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/3/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "3"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/4/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "4"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/5/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "5"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/6/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "6"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/7/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "7"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/8/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "8"
+
+python forecasting.py --folder_path_to_dataset "./Cameroon/dataset" --folder_path_to_workspace "./Cameroon/7_provinces/out_hyper/9/out_test" --folder_path_to_hyperparameters "./Cameroon/7_provinces/out_hyper" --hyperparameters_session "9"
+
+
